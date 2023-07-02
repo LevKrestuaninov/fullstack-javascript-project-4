ed5dbf1b8dbd5fc865326dd1ab773a4ccab2084f
LevKrestuaninov <krestuaninovlev@gmail.com>
2023-07-02 16:23:03 +0300

    add workflow and eslint

--- Makefile
+++ Makefile
@@ -2,3 +2,7 @@ install:
 	npm ci
 lint:
 	npx eslint .
+publish:
+	npm publish --dry-run
+pageLoader:
+	node bin/pageLoader.js
---
+++ bin/pageLoader.js
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .name('gendiff ')
+  .description(' Compares two configuration files and shows a difference.')
+  .version('0.8.0')
+
+  .argument('<filepath1>', 'path to file 1')
+  .argument('<filepath2>', 'path to file 1')
+  .option('-f, --format <type>', 'output format', 'stylish');
+/*
+  .action((filepath1, filepath2) => {
+    const options = program.opts();
+    console.log();
+  });
+  */
+
+program.parse();
